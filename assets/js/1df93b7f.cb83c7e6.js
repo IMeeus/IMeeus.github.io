3930872d0e37bc2c7dc94eea7e55d4484b7a56b1
IMeeus <maeleeuxs@gmail.com>
2024-07-10 23:17:49 +0200

    Deploy website - based on 584782fcf25615a57d955d9f925041998d6f5a3c

---
+++ assets/js/1df93b7f.cb83c7e6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkimeeus_site=self.webpackChunkimeeus_site||[]).push([[583],{6866:(e,s,u)=>{u.r(s),u.d(s,{default:()=>r});var t=u(6347),i=u(4848);function r(){return(0,i.jsx)(t.rd,{to:"/blog"})}}}]);
---
+++ assets/js/814f3328.91bad699.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkimeeus_site=self.webpackChunkimeeus_site||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Reaching mockist nirvana","permalink":"/blog/mockist-nirvana","unlisted":false}]}')}}]);
